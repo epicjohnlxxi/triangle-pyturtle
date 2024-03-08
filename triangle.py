73357824370ba35d1bf22c4266cac36320ed2bd4
john fiore <142550056+epicjohnlxxi@users.noreply.github.com>
2024-03-08 16:36:23 -0600

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,17 @@
+# made by epicjohn
+# mar. 8, 2024
+
+import turtle
+
+t = turtle.Turtle()
+t.speed(5)
+t.left(65)
+t.pendown()
+t.forward(100)
+t.right(130)
+t.forward(100)
+t.left(245)
+t.forward(85)
+t.penup()
+t.hideturtle()
+t.done()
